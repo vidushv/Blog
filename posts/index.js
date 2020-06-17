739630f797c9ce00f074a3d616ca63c6e9b7d911
vidushv <vidushv@gmail.com>
2020-06-16 23:48:51 -0700

    Add cors

--- posts/index.js
+++ posts/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
+
+app.use(bodyParser.json());
+app.use(cors());
 
 const app = express();
 
