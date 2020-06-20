2ff4711eeb7a1483d55c6bb8d66675d5e06d0323
vidushv <vidushv@gmail.com>
2020-06-19 19:02:44 -0700

    Add async post to event-bus service when comment is created

--- comments/index.js
+++ comments/index.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { randomBytes } = require('crypto');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -9,24 +10,34 @@ app.use(cors());
 
 const commentsByPostId = {};
 
-app.get('/posts/:id/comments', (req,res) => {
-    //Guard against empty comment array in case there are no comments.
-    res.send(commentsByPostId[req.params.id] || []);
+app.get("/posts/:id/comments", (req, res) => {
+  //Guard against empty comment array in case there are no comments.
+  res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req,res) => {
-    const commentId = randomBytes(4).toString('hex');
-    const { content } = req.body;
+app.post("/posts/:id/comments", async (req, res) => {
+  const commentId = randomBytes(4).toString("hex");
+  const { content } = req.body;
 
-    //If there are no comments, this will return undefined.
-    //In that case, return empty array [].
-    const comments = commentsByPostId[req.params.id] || [];
-    comments.push({ id: commentId, content }); 
-    commentsByPostId[req.params.id] = comments;
-    
-    res.status(201).send(comments);
+  //If there are no comments, this will return undefined.
+  //In that case, return empty array [].
+  const comments = commentsByPostId[req.params.id] || [];
+  comments.push({ id: commentId, content });
+  commentsByPostId[req.params.id] = comments;
+
+  //Add async post to event bus when comment is created.
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+    },
+  });
+
+  res.status(201).send(comments);
 });
 
 app.listen(4001, () => {
-    console.log('Listening on 4001');
-})
+  console.log("Listening on 4001");
+});
